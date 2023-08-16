21f2fe9252996596f6c740bc32ad2130cb87ed08
ShayGuedj1 <112567652+ShayGuedj1@users.noreply.github.com>
2023-08-16 11:08:02 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,17 +3,8 @@ import password
 import errors
 
 user = username.user1
-
 pwd = password.pass1
 
-# userillegalchar = errors.UsernameContainsIllegalCharacter()
-# usertooshort = errors.UsernameTooShort()
-# usertoolong = errors.UsernameTooLong()
-# passinvalidchar = errors.PasswordMissingCharacter()
-# passtooshot = errors.PasswordTooShort()
-# passtoolong = errors.PasswordTooLong()
-# This function checks that all the parameters are correct. If somthing is miising,
-# it will alert using the try and exept.
 def check_input(username, password):
     # If the passowrd and username are correct, it will print ""
     if username.username_length() and username.has_digit() and username.has_underscore() and username.is_alpha() and \
